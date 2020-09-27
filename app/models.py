ed6991c201bb4014d27bdd9931ed8b605834a506
MikeC <kiragumike@gmail.com>
2020-09-27 03:34:50 +0300

    added a Post class

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 
 class User(db.Model):
@@ -15,5 +16,31 @@ class User(db.Model):
 
     password_secure = db.Column(db.String(255))
 
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self,password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure,password) 
+
+
     def __repr__(self):
         return f'{self.username}'
+
+        
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(50))
+    subtitle = db.Column(db.String(50))
+    comments = db.relationship('Comment',backref = 'post', lazy = 'dynamic') 
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+    content = db.Column(db.Text)       
