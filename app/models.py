c712009c667be734623cb5881356657683fe1339
MikeC <kiragumike@gmail.com>
2020-09-27 05:40:02 +0300

    added wtf forms

--- app/models.py
+++ app/models.py
@@ -1,9 +1,16 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
+from . import login_manager
 
 
-class User(db.Model):
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))    
+
+
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key = True)
@@ -88,6 +95,4 @@ class Subscriber(db.Model):
        return f'User {self.email}'
 
 
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return User.query.get(int(user_id))     
+ 
